a1669d9367a82fc613fd8cef19b0c7373c9426ed
mostafahatem1 <mostafahatem760@gmail.com>
2025-10-10 23:01:01 +0300

    Add Genres

--- config/session.js
+++ config/session.js
@@ -1,21 +1,29 @@
 import session from "express-session";
 import { RedisStore } from "connect-redis";
 import { redisClient } from "./db.js";
-let redisStore = new RedisStore({
-    client: redisClient,
-    prefix: "myapp:",
-});
+const createSessionMiddleware = () => {
+    if (!process.env.SESSION_SECRET) {
+        throw new Error("❌ SESSION_SECRET is required in .env file!");
+    }
 
-const sessionConfig = {
-    store: redisStore,
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 24 * 60 * 60 * 1000,
-    },
+    const redisStore = new RedisStore({
+        client: redisClient,
+        prefix: "myapp:",
+    });
+
+    const sessionConfig = {
+        store: redisStore,
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 24 * 60 * 60 * 1000,
+        },
+    };
+
+    return session(sessionConfig); 
 };
-const appSession = session(sessionConfig);
-export default appSession;
+
+export default createSessionMiddleware;
