4ad2c54a39f411717edceda2994e5368d3b69fa8
Mohamedhaytham9 <mohamed_haytham999@outlook.com>
2025-10-13 04:18:21 +0300

    created profile contreller which retrieve profile details and update the profile , also updated the books routes

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ import HTTPStatusText from "./utils/HTTPStatusText.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import createSessionMiddleware from "./config/session.js";
 import { morganLogger } from "./middlewares/morgan.js"
+import profileRouter  from "./routes/profile.js";
 const bootstrap = async () => {
     const app = express();
     const PORT = process.env.PORT || 3000;
     await connectMongoDB();
-    await connectRedisDB();
+    //await connectRedisDB();
     app.use(createSessionMiddleware());
     app.use(express.json());
     app.use(morganLogger);
     app.use("/api/auth", authRoutes);
     app.use("/api/genres", genreRoutes);
     app.use("/api/books", bookRouter);
+    app.use("/api/users", profileRouter);
     app.all("/{*dummy}", (req, res, next) => {
         res.status(404).json({
             message: "Route Not Found",
--- routes/book.js
+++ routes/book.js
@@ -1,25 +1,14 @@
 import express from "express";
-import {
-    AddBook,
-    getBooks,
-    getBookByID,
-    updateBook,
-    deleteBook,
-} from "../controllers/BooksController.js";
+import {AddBook,getBooks,getBookByID,updateBook,deleteBook,} from "../controllers/BooksController.js";
 import { authenticate } from "../middlewares/auth.js";
 import { authorize } from "../middlewares/authorization.js";
 
 const router = express.Router();
 
-router.post("/", authenticate, authorize("admin", "author"), AddBook);
-router.get("/", getBooks);
-router.get("/:id", getBookByID);
-router.put("/:id", authenticate, authorize("admin", "librarian"), updateBook);
-router.delete(
-    "/:id",
-    authenticate,
-    authorize("admin", "librarian"),
-    deleteBook
-);
+router.post("/Create-Book", authenticate, authorize("admin", "author"), AddBook);
+router.get("/List-Books", getBooks);
+router.get("/Get-Book/:id", getBookByID);
+router.put("/Update-Book/:id", authenticate, authorize("admin", "author"), updateBook);
+router.delete("/Delete/:id",authenticate,authorize("admin", "author"),deleteBook);
 
 export default router;
