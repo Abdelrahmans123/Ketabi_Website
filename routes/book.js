044bad9ec666d66a4bf16ec7d09b759cf1a1aa2b
Mohamedhaytham9 <mohamed_haytham999@outlook.com>
2025-10-18 22:43:56 +0300

    added Pagination, Sorting, Filtration to Books, Also added needed Validations to it

--- routes/book.js
+++ routes/book.js
@@ -3,13 +3,15 @@ import {AddBook,getBooks,getBookByID,updateBook,deleteBook,} from "../controller
 import { authenticate } from "../middlewares/auth.js";
 import { authorize } from "../middlewares/authorization.js";
 import upload from "../middlewares/upload.js";
+import {validate}  from "../middlewares/validation.js";
+import { createSchema, updateSchema } from "../validations/book.js";
 
 const router = express.Router();
 
-router.post("/Create-Book", authenticate, authorize("admin", "author"), upload.single("pdf"), AddBook);
+router.post("/Create-Book", authenticate, authorize("admin", "author"), upload.single("pdf"),validate(createSchema),AddBook);
 router.get("/List-Books", getBooks);
 router.get("/Get-Book/:id", getBookByID);
-router.put("/Update-Book/:id", authenticate, authorize("admin", "author"), updateBook);
+router.put("/Update-Book/:id", authenticate, authorize("admin", "author"), validate(updateSchema), updateBook);
 router.delete("/Delete/:id",authenticate,authorize("admin", "author"),deleteBook);
 
 export default router;
