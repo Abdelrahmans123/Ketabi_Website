481956604b2fcbcbf2010d8b379c5ec55712c809
Abdelrahman Salah <sabdelrahman110@gmail.com>
2025-10-29 02:19:37 +0300

    refactor: update query parameter in updateOne calls for consistency

--- routes/book.js
+++ routes/book.js
@@ -112,23 +112,48 @@
  *         description: Book deleted successfully
  */
 
-
 import express from "express";
-import {AddBook,getBooks,getBookByID,updateBook,deleteBook,downloadBook} from "../controllers/BooksController.js";
+import {
+    AddBook,
+    getBooks,
+    getBookByID,
+    updateBook,
+    deleteBook,
+    downloadBook,
+} from "../controllers/BooksController.js";
 import { authenticate } from "../middlewares/auth.js";
 import { authorize } from "../middlewares/authorization.js";
 import upload from "../middlewares/upload.js";
-import {validate}  from "../middlewares/validation.js";
+import { validate } from "../middlewares/validation.js";
 import { createSchema, updateSchema } from "../validations/book.js";
 import { roleEnum } from "../utils/roleEnum.js";
+import { cacheMiddleware } from "../middlewares/cach.js";
 
 const router = express.Router();
 
-router.post("/Create-Book",authenticate, authorize(roleEnum.publisher),validate(createSchema), upload.single("pdf"),AddBook);
-router.get("/List-Books", getBooks);
-router.get("/Get-Book/:id", getBookByID);
-router.put("/Update-Book/:id", authenticate, authorize("admin", "author"), validate(updateSchema), updateBook);
-router.delete("/Delete/:id",authenticate,authorize("admin", "author"),deleteBook);
+router.post(
+    "/Create-Book",
+    authenticate,
+    authorize(roleEnum.publisher),
+    validate(createSchema),
+    upload.single("pdf"),
+    AddBook
+);
+router.get("/List-Books", cacheMiddleware("List-Books"), getBooks);
+router.get("/Get-Book/:id", cacheMiddleware("Get-Book"), getBookByID);
+router.put(
+    "/Update-Book/:id",
+    authenticate,
+    authorize("admin", "author"),
+    validate(updateSchema),
+    updateBook
+);
+router.delete(
+    "/Delete/:id",
+    authenticate,
+    authorize("admin", "author"),
+    deleteBook
+);
 router.get("/Download-Book/:id", authenticate, downloadBook);
 
 export default router;
